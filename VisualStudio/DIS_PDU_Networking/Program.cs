09b0a0a6ae29dee385830371a0ba513ae1e1f229
bailey halliday <you@example.com>
2024-06-14 14:06:35 +0100

    Initial commit

---
+++ VisualStudio/DIS_PDU_Networking/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DIS_PDU_Networking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
